b300806dd596d7f9d1db9743659a013f6a9d4171
Nathan Dai <nathandai2000@gmail.com>
2022-11-28 18:33:51 -0800

    starter code

---
+++ src/AlienHorde.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlienHorde {
+	private List<Alien> aliens;
+
+	public AlienHorde(int size) {
+	}
+
+	public void add(Alien al) {
+	}
+
+	public void draw(Graphics window) {
+	}
+
+	public void move() {
+	}
+
+	// calculate if Aliens are hit by shots, if so remove the shot and alien and return the number of hits
+	public int calcHits(List<Ammo> shots) {
+		return 0;
+	}
+
+	public String toString() {
+		return "";
+	}
+}
---
+++ src/Bullets.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bullets {
+	private List<Ammo> ammo;
+
+	public Bullets() {
+	}
+
+	public void add(Ammo al) {
+	}
+
+	//post - draw each Ammo
+	public void draw(Graphics window) {
+	}
+
+	public void move() {
+	}
+
+	// remove any Ammo which has reached the edge of the screen
+	public void cleanUpEdges() {
+	}
+
+	public List<Ammo> getList() {
+		return null;
+	}
+
+	public String toString() {
+		return "";
+	}
+}
